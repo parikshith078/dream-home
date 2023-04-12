67d389db2d21ac2c532fd5cda2ece8969a75a403
parikshith078 <palegar.parikshith@gmail.com>
2023-04-12 17:44:08 +0530

    import bugfix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaUserAlt } from "react-icons/fa";
-import { LeaseIndex } from "../data/infoFile";
 
 const NavBar = () => {
   return (
