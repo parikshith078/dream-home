091548ebea68d9b547394437c4a7bb52f82d9f6a
parikshith078 <palegar.parikshith@gmail.com>
2023-04-14 16:23:23 +0530

    build errors

--- app/register/[id]/page.tsx
+++ app/register/[id]/page.tsx
@@ -1,15 +1,10 @@
-import ClientForm from "../../../components/ClientForm";
 import RegistrationForm from "../../../components/RegistrationForm";
-import StaffFrom from "../../../components/StaffForm";
-import { FromType } from "../../../data/infoFile";
 
 export default function FormPage({ params }: any) {
   const { id } = params;
   return (
     <>
       <RegistrationForm query={id} />
-      {/* <StaffFrom ={FromType[id]} /> */}
-      {/* <ClientForm /> */}
     </>
   );
 }
